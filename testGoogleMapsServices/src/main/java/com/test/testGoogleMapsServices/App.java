4357460610952e2aeb2519e46990cda00a374e97
Brett Morgan <brettmorgan@google.com>
2017-04-26 20:14:38 +1000

    And the rest of it

--- testGoogleMapsServices/src/main/java/com/test/testGoogleMapsServices/App.java
+++ testGoogleMapsServices/src/main/java/com/test/testGoogleMapsServices/App.java
@@ -1,10 +1,12 @@
 package com.test.testGoogleMapsServices;
 
-import com.google.maps.GaeRequestHandler;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.GeocodingApiRequest;
 import com.google.maps.model.GeocodingResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * Hello world!
@@ -12,44 +14,31 @@ import com.google.maps.model.GeocodingResult;
  */
 public class App {
 
+	static private Logger LOG;
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		LOG = LoggerFactory.getLogger(App.class);
+    	LOG.info("Hello World");
 		App app = new App();
 		app.updateLatLng("London");
 	}
 
 	public void updateLatLng(String address) {
-		String googleMapsKey = "your key";
-
-		GeoApiContext context = new GeoApiContext(new GaeRequestHandler()).setApiKey(googleMapsKey);
-		System.out.println(googleMapsKey);
+		String googleMapsKey = "YOUR_API_KEY_HERE";
+		LOG.info("Creating context");
+		GeoApiContext context = new GeoApiContext().setApiKey(googleMapsKey);
+		LOG.info("Creating request");
 		GeocodingApiRequest req = GeocodingApi.newRequest(context).address(address + ", ");
 
 		// Synchronous
 		try {
+			LOG.info("Sending request");
 			GeocodingResult[] geocodingResultArray = req.await();
+			LOG.info("Result received");
 			// Handle successful request.
-			System.out.println(geocodingResultArray.length);
+			LOG.info(geocodingResultArray[0].formattedAddress);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		// Synchronous ignore error
-		// req.awaitIgnoreError(); // No checked exception.
-
-		// Async
-		// req.setCallback(new PendingResult.Callback<GeocodingResult[]>() {
-		// @Override
-		// public void onResult(GeocodingResult[] geocodingResultArray) {
-		// // Handle successful request.
-		// System.out.println(geocodingResultArray.length);
-		// }
-		//
-		// @Override
-		// public void onFailure(Throwable e) {
-		// // Handle error.
-		// System.out.println("ERROR");
-		// }
-		// });
 	}
 }
